124129569a1d632fdb9ecae4534a912f149192e4
phage <kyp0717@gmail.com>
2021-07-23 11:35:17 -0400

    first

---
+++ stream.py
@@ -0,0 +1,41 @@
+### import
+import logging
+
+from alpaca_trade_api.stream import Stream
+
+log = logging.getLogger(__name__)
+
+### async stuff
+async def print_trade(t):
+    print('trade', t)
+
+
+async def print_quote(q):
+    print('quote', q)
+
+
+async def print_trade_update(tu):
+    print('trade update', tu)
+
+### main
+def main():
+    logging.basicConfig(level=logging.INFO)
+    feed = 'iex'  # <- replace to SIP if you have PRO subscription
+    stream = Stream(data_feed=feed, raw_data=True)
+    stream.subscribe_trade_updates(print_trade_update)
+    stream.subscribe_trades(print_trade, 'AAPL')
+    stream.subscribe_quotes(print_quote, 'IBM')
+
+    @stream.on_bar('MSFT')
+    async def _(bar):
+        print('bar', bar)
+
+    @stream.on_status("*")
+    async def _(status):
+        print('status', status)
+
+    stream.run()
+
+
+if __name__ == "__main__":
+    main()
